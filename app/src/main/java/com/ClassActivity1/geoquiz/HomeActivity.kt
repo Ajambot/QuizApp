bd7d8a60dc301b2ea88f45b3e752f4420e7b0317
Martin Morales <mmorale5@lakeheadu.ca>
2025-11-06 18:12:30 -0500

    Add non-working home page

---
+++ app/src/main/java/com/ClassActivity1/geoquiz/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.ClassActivity1.geoquiz
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.ClassActivity1.geoquiz.databinding.ActivityHomeBinding
+
+import kotlin.math.ceil
+import android.os.SystemClock
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
